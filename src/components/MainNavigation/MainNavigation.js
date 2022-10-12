b1743fa4e6eac9043421f4e76b2facc677f597f5
Mohammad Sabir <mohammad.sabir@ariat.com>
2022-10-12 18:09:29 +0530

    Fix active nav link class

--- src/components/MainNavigation/MainNavigation.js
+++ src/components/MainNavigation/MainNavigation.js
@@ -1,10 +1,11 @@
 import { NavLink } from "react-router-dom";
 const MainNavigation = (props) => {
     const {toggleNavigation} = props;
+    const location = window.location.pathname;
     return (
         <ul id="menu-main" className={`white-link dropdown-dark text-lg-center nav-menu ${props.classes}` } style={{display: toggleNavigation ? 'block' : 'none'}}>
-            <li className="has-dropdown"><NavLink activeClassName="active" to="/"> Home </NavLink></li>
-            <li className="has-dropdown"><NavLink to="/recipes">Recipes</NavLink></li>
+            <li  className={`has-dropdown ${location === "/" ? "active" : ""}`}><NavLink activeClassName="active" to="/"> Home </NavLink></li>
+            <li className={`has-dropdown ${location === "/recipes" ? "active" : ""}`}><NavLink to="/recipes">Recipes</NavLink></li>
         </ul>
     )
 }
