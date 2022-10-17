38fec3c077a0c848bfc6242753f6c57dbddb7378
Mohammad Sabir <mohammad.sabir@ariat.com>
2022-10-17 16:02:31 +0530

    Refactor router code

--- src/components/MainNavigation/MainNavigation.js
+++ src/components/MainNavigation/MainNavigation.js
@@ -4,8 +4,8 @@ const MainNavigation = (props) => {
     const location = window.location.pathname;
     return (
         <ul id="menu-main" className={`white-link dropdown-dark text-lg-center nav-menu ${props.classes}` } style={{display: toggleNavigation ? 'block' : 'none'}}>
-            <li  className={`has-dropdown ${location === "/" ? "active" : ""}`}><NavLink activeClassName="active" to="/"> Home </NavLink></li>
-            <li className={`has-dropdown ${location === "/recipes" ? "active" : ""}`}><NavLink to="/recipes">Recipes</NavLink></li>
+            <li><NavLink activeClassName="active" exact to="/"> Home </NavLink></li>
+            <li><NavLink  activeClassName="active" to="/recipes">Recipes</NavLink></li>
         </ul>
     )
 }
