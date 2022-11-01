353d3e4723e1dfc31a859302335c32aab27b700f
sabir <sabir413@gmail.com>
2022-11-01 18:06:32 +0530

    Add Lazy load

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,28 +1,35 @@
+import React, {Suspense, lazy} from 'react';
 import { Switch } from 'react-router-dom';
-import HomePage from '../pages/HomePage';
-import Recipes from '../pages/Recipes';
-import RecipeDetails from '../pages/RecipeDetails';
-import Signup from '../pages/Signup';
-import Signin from '../pages/Singin';
-import NotFound from '../pages/NotFound';
-import InnerPageRoute from './InnerPageRoute';
-import MasterPageRoute from './MasterPageRoute';
-import AddRecipe from '../pages/AddRecipe';
-import Profile from '../pages/Profile';
-import ForgotPassword from '../pages/ForgotPassword';
+import Loading from "react-fullscreen-loading";
+
+const HomePage = lazy(() => import('../pages/HomePage'));
+const Recipes = lazy(() => import('../pages/Recipes'));
+const RecipeDetails = lazy(() => import('../pages/RecipeDetails'));
+const Signup = lazy(() => import('../pages/Signup'));
+const Signin = lazy(() => import('../pages/Singin'));
+const NotFound = lazy(() => import('../pages/NotFound'));
+const InnerPageRoute = lazy(() => import('./InnerPageRoute'));
+const MasterPageRoute = lazy(() => import('./MasterPageRoute'));
+const AddRecipe = lazy(() => import('../pages/AddRecipe'));
+const Profile = lazy(() => import('../pages/Profile'));
+const ForgotPassword = lazy(() => import('../pages/ForgotPassword'));
 const Routers = () => {
     return (
         <Switch>
-            <MasterPageRoute path='/' exact component={HomePage}/>
-            <InnerPageRoute path='/recipes' exact component={Recipes} />
-            <InnerPageRoute path='/recipes/:id' component={Recipes} />
-            <InnerPageRoute path='/recipeDetails/:id' component={RecipeDetails} />
-            <InnerPageRoute path='/singup' component={Signup} />
-            <InnerPageRoute path='/signin' component={Signin} />
-            <InnerPageRoute path='/profile' component={Profile} isProtected/>
-            <InnerPageRoute path='/forgot-password' component={ForgotPassword} />
-            <InnerPageRoute path="/addrecipe" component={AddRecipe} isProtected/>
-            <InnerPageRoute path="/*" component={NotFound} />
+            <Suspense fallback={
+                <Loading loading={true} background="#ffffffb3" loaderColor="#b1b1b1" />
+            }>
+                <MasterPageRoute path='/' exact component={HomePage}/>
+                <InnerPageRoute path='/recipes' exact component={Recipes} />
+                <InnerPageRoute path='/recipes/:id' component={Recipes} />
+                <InnerPageRoute path='/recipeDetails/:id' component={RecipeDetails} />
+                <InnerPageRoute path='/singup' component={Signup} />
+                <InnerPageRoute path='/signin' component={Signin} />
+                <InnerPageRoute path='/profile' component={Profile} isProtected/>
+                <InnerPageRoute path='/forgot-password' component={ForgotPassword} />
+                <InnerPageRoute path="/addrecipe" component={AddRecipe} isProtected/>
+                <InnerPageRoute path="/*" component={NotFound} />
+            </Suspense>
         </Switch>
     )
 };
